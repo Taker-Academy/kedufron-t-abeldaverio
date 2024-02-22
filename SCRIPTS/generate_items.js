0458532081ec664582f5b2c1bc25606ea9897a29
abeldaverio <abel.daverio@epitech.eu>
2024-02-22 18:26:04 +0100

    [UPD] index page

--- SCRIPTS/generate_items.js
+++ SCRIPTS/generate_items.js
@@ -2,15 +2,13 @@
 function insert_element(item, list) {
     const li = document.createElement("li");
     var a = document.createElement("a");
-    a.href = (document.URL + "/" + item._id);
+    a.href = (document.URL.slice(0, document.URL.length - "index.html".length) + ("item.html?id=" + item._id));
     const article = document.createElement("article");
     var name = document.createElement("h2");
-    const name_text = document.createTextNode(item.name);
-    name.appendChild(name_text);
+    name.appendChild(document.createTextNode(item.name));
     name.classList.add("article_text");
     var price = document.createElement("p");
-    const price_text = document.createTextNode(item.price + "\u20AC");
-    price.appendChild(price_text);
+    price.appendChild(document.createTextNode(item.price + "€"));
     price.classList.add("article_text");
     var image = document.createElement("img");
     image.src = ("https://api.kedufront.juniortaker.com/item/picture/" + (item.image));
@@ -25,6 +23,7 @@ function insert_element(item, list) {
 function get_elements() {
     axios.get("https://api.kedufront.juniortaker.com/item/")
         .then(response => {
+            console.log(response.data);
             const items = response.data;
             const list = document.getElementById("product_list");
             for (let i = 0; i < items.length; i++) {
